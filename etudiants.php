0bd046fef7fef69b0f14d0a22925df83a45468cd
Philippe Kurz <philippe.kurz@gmail.com>
2025-03-10 17:10:06 +0100

    Crud étudiants

--- etudiants.php
+++ etudiants.php
@@ -15,5 +15,43 @@
         require_once 'composants/menu.php'; // On inclut le menu
     ?>
     <h1>Etudiants</h1>
+    <a href="">Ajouter un étudiant</a>
+    <br><br>
+    
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Email</th>
+            <th>Téléphone</th>
+            <th>Admin</th>
+            <th>Etudiant</th>
+            <th>Professeur</th> 
+            <th></th>   
+        </tr>
+
+        <?php
+            require_once 'config.php'; // On inclut la connexion à la base de données
+            $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+
+            $requete = $pdo->query('SELECT * FROM personnes ORDER BY nom, prenom;');
+            while ($data = $requete->fetch()) {
+                echo '<tr>';
+                echo '<td>' . $data['id'] . '</td>';
+                echo '<td>' . $data['nom'] . '</td>';
+                echo '<td>' . $data['prenom'] . '</td>';
+                echo '<td>' . $data['email'] . '</td>';
+                echo '<td>' . $data['telephone'] . '</td>';
+                echo '<td>' . $data['admin'] . '</td>';
+                echo '<td>' . $data['etudiant'] . '</td>';
+                echo '<td>' . $data['professeur'] . '</td>';
+                echo '<td><a href="">Modifier</a>&nbsp;<a href="">Supprimer</a></td>';
+                echo '</tr>';
+            }
+        ?>
+
+    </table>
+
 </body>
 </html>
